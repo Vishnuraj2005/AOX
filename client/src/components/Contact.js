985ca39d763450b0f31671b8974d5e43740ada0a
Vishnuraj2005 <vr926156@gmail.com>
2026-04-14 13:28:13 +0530

    Commit

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -49,7 +49,7 @@ const Contact = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/contact', formData);
+      const response = await axios.post('https://aox.onrender.com/api/contact', formData);
       if (response.status === 201) {
         setSubmitted(true);
         setTimeout(() => setSubmitted(false), 3000);
