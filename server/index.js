52ffbc9afbaa501910c350ed87babc647298776f
Vishnuraj2005 <vr926156@gmail.com>
2026-04-14 15:26:00 +0530

    Commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://polite-yeot-c9e097.netlify.app', 'http://localhost:3000']
+}));
 app.use(bodyParser.json());
 
 // Database Connection
